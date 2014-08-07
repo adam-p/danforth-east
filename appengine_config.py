df1bfdf87930e978dc7d08d87a6b32e2c0a3095e
Adam Pritchard <pritchard.adam@gmail.com>
2014-08-07 06:21:16 -0400

    Added ereporter

--- appengine_config.py
+++ appengine_config.py
@@ -4,3 +4,8 @@ import os.path
 # add `lib` subdirectory to `sys.path`, so our `main` module can load
 # third-party libraries.
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
+
+
+# Register ereporter
+from google.appengine.ext import ereporter
+ereporter.register_logger()
