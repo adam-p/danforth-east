f2cb8a0d8a3b256935523311494c4c04fa9060a9
Adam Pritchard <pritchard.adam@gmail.com>
2014-10-13 23:40:42 -0400

    Volunteer UI work in progress

---
+++ static/js/self-serve-combo.js
@@ -0,0 +1,24 @@
+/*
+ * Copyright Adam Pritchard 2014
+ * MIT License : http://adampritchard.mit-license.org/
+ */
+
+$(function() {
+  "use strict";
+
+  $('.mode-chooser button').click(function() {
+    setMode(this.value);
+    $('.mode-chooser').hide();
+  });
+
+  function setMode(mode) {
+    if (mode === 'member') {
+      $('#newMember').show();
+      $('#newVolunteer').hide();
+    }
+    else { // volunteer
+      $('#newMember').hide();
+      $('#newVolunteer').show();
+    }
+  }
+});
